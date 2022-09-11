ca36d65a3ea0716d2f9e406dd7c191061c39928a
Sávio Freire <freiresavio7@gmail.com>
2022-09-11 13:51:03 -0300

    pronto

--- javas.js
+++ javas.js
@@ -1,30 +1,67 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
+fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')//puxando a api
         .then(response => response.json())
         .then(allpokemon => {
 
-            let pokemon = []
-            const telinha = document.querySelector('#telinha');
+            let pokemon = []                                    //onde armazeno o nome a foto da pokemon
+            const telinha = document.querySelector('#telinha'); //local da tela
+            const bt = document.querySelectorAll('#bt .bt');//local dos botoes
+            let numero = document.querySelector('#input input');
+            
+            
 
-            allpokemon.results.map((val) =>{
+            allpokemon.results.map((val) =>{                    //puxando a api que esta dentro da api principal
                 
                 fetch(val.url)
                 .then(response => response.json())
                 .then(pokemonSingle=> {
                     
-                pokemon.push({nome:val.name ,imagem:pokemonSingle.sprites.front_default})
+                pokemon.push({nome:val.name ,imagem:pokemonSingle.sprites.front_default})//colocando o nome e a imagem do pokemon dentro da minha array pokemon
+                var i = 0 
+
+
+                    telinha.innerHTML =`
+                        <h1 class="titulo">${pokemon[i].nome}</h1>
+                        <div class="fotinha">
+                        <img src="${pokemon[i].imagem}" alt="poke">
+                        </div>  
+                        `
+
+
+                    bt[0].addEventListener('click',()=>{
+                        i--
+                        telinha.innerHTML =`
+                            <h1 class="titulo">${pokemon[i].nome}</h1>
+                            <div class="fotinha">
+                            <img src="${pokemon[i].imagem}" alt="poke">
+                            </div>  `
+                        console.log(i)
+                    })
+
+                    bt[1].addEventListener('click',()=>{
+                        i++
+                        telinha.innerHTML =`
+                            <h1 class="titulo">${pokemon[i].nome}</h1>
+                            <div class="fotinha">
+                            <img src="${pokemon[i].imagem}" alt="poke">
+                            </div>  `
+                        console.log(i)
+                    })
                     
-                setInterval(() => {
                     
-                telinha.innerHTML =`
-                <h1 class="titulo">${pokemon[i].nome}</h1>
-                <div class="fotinha">
-                    <img src="${pokemon[i].imagem}" alt="poke">
-                </div> 
-                `
-                i++
-            }, 5000);
+                    numero.addEventListener('keyup',()=>{
+                        i = numero.value
+                        telinha.innerHTML =`
+                            <h1 class="titulo">${pokemon[i].nome}</h1>
+                            <div class="fotinha">
+                            <img src="${pokemon[i].imagem}" alt="poke">
+                            </div>  `
+                    })
+                
+                      
+            
                 
                 })
                 
             })
         })
+        
