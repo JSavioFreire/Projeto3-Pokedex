d5a20f637a124144c302130d78ce03286e4f86fc
Sávio Freire <freiresavio7@gmail.com>
2022-09-11 12:10:44 -0300

    indo

--- javas.js
+++ javas.js
@@ -2,18 +2,27 @@ fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
         .then(response => response.json())
         .then(allpokemon => {
 
-            pokemon = []
-
-            
+            let pokemon = []
+            const telinha = document.querySelector('#telinha');
 
             allpokemon.results.map((val) =>{
                 
                 fetch(val.url)
                 .then(response => response.json())
                 .then(pokemonSingle=> {
-        
+                    
                 pokemon.push({nome:val.name ,imagem:pokemonSingle.sprites.front_default})
-
+                    
+                setInterval(() => {
+                    
+                telinha.innerHTML =`
+                <h1 class="titulo">${pokemon[i].nome}</h1>
+                <div class="fotinha">
+                    <img src="${pokemon[i].imagem}" alt="poke">
+                </div> 
+                `
+                i++
+            }, 5000);
                 
                 })
                 
